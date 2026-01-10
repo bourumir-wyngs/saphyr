540a6667c67ebc571e34edeac743909e9e5d49ae
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2026-01-10 22:22:00 +0100

    5LLU block scalar indentation parser test

--- parser/tests/block_scalar_indentation.rs
+++ parser/tests/block_scalar_indentation.rs
@@ -2,20 +2,35 @@ use saphyr_parser::{Event, Parser};
 
 #[test]
 fn yaml_5llu_block_scalar_wrong_indent_should_fail() {
-    // block scalars require consistent indentation.
-    let yaml = "block scalar: >\n\n  \n   \n    invalid\n";
+    // It is ONLY an error for any of the leading empty lines to contain MORE spaces than the first non-empty line.
+    let yaml_ok = "block scalar: >\n\n  \n   \n    invalid\n";
 
-    let mut parser = Parser::new_from_str(yaml);
+    let mut parser = Parser::new_from_str(yaml_ok);
 
     while let Some(next) = parser.next() {
         match next {
             Ok((Event::DocumentEnd, _)) => {
-                assert!(false, "Document end before any error");
+                break; // fine
             }
             Err(err) => {
-                assert_eq!(err.info(), "misplaced bracket",
-                           "5LLU: block scalars require consistent indentation"
-                );
+                assert!(false, "{} reported for valid YAML", err);
+                break;
+            }
+            _ => {}
+        }
+    }
+
+    // It IS an error for any of the leading empty lines to contain MORE spaces than the first non-empty line.
+    let yaml_invalid = "block scalar: >\n\n                             \n   \n    invalid\n";
+
+    let mut parser = Parser::new_from_str(yaml_invalid);
+
+    while let Some(next) = parser.next() {
+        match next {
+            Ok((Event::DocumentEnd, _)) => {
+                assert!(false, "Document end before any error, invalid YAML");
+            }
+            Err(err) => {
                 break; // fine
             }
             _ => {}
